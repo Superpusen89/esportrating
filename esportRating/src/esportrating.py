585ef1dc288f01e1dceb35bdbe330bb5cf09fe0a
Henriette <superpusen89@gmail.com>
2015-03-10 14:44:14 +0100

    getplayers + database

--- esportRating/src/esportrating.py
+++ esportRating/src/esportrating.py
@@ -1,9 +1,39 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+from flask import Flask
+from flask.ext.restful import Api, Resource
+import MySQLdb
 
-__author__ = "henriette"
-__date__ = "$02.mar.2015 10:43:34$"
+app = Flask(__name__)
+api = Api(app)
 
-if __name__ == "__main__":
-    print "Hello World"
+try:
+    conn = MySQLdb.connect(host="localhost", user="root", passwd="HenrietteIda", db="esportrating")
+    cursor = conn.cursor()
+    
+except MySQLdb.Error, e:
+    print "Error %d: %s" % (e.args[0], e.args[1])
+    sys.exit(1)
+    
+
+class HelloWorld(Resource):
+    def get(self):
+        return 'Hello world'
+
+class GetPlayers(Resource):
+    def get(self):
+        cursor.execute("select * from Player")
+        data = cursor.fetchall()
+        print data
+            
+        
+
+api.add_resource(GetPlayers, '/getplayers')
+api.add_resource(HelloWorld, '/')
+
+
+
+cursor.execute("select * from Player")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",
+        port=int("5001"))
