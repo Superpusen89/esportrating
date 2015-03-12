7ea45b77a0009d74f72eaf3a574168b2d0810f2c
Henriette <superpusen89@gmail.com>
2015-03-12 09:27:15 +0100

    Tester mulighet for å kunne legge til spillere med å motta et JSONobjekt

--- esportRating/src/esportrating.py
+++ esportRating/src/esportrating.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask.ext.restful import Api, Resource
+from flask import Flask, request, json
+from flask.ext.restful import Api, Resource, reqparse, fields, marshal
 import MySQLdb
 
 app = Flask(__name__)
@@ -13,11 +13,39 @@ except MySQLdb.Error, e:
     print "Error %d: %s" % (e.args[0], e.args[1])
     sys.exit(1)
     
-
+    
+    
+    
+    
 class HelloWorld(Resource):
     def get(self):
         return 'Hello world'
 
+#player_fields = {
+ #   'player_id': fields.Integer,
+  #  'username': fields.String,
+   # 'team_id': fields.Integer
+#}
+
+
+class Player(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('player_id', type=int, location='json')
+        self.reqparse.add_argument('username', type=str, location='json')
+        self.reqparse.add_argument('team_id', type=int, location='json')
+        super(Player, self).__init__()
+        
+    def get(self):
+        cursor.execute("select * from Player") # where username = %s", (user_name))
+        data = cursor.fetchall()
+        return data
+    
+    def post(self):
+        args = self.reqparse.parse_args()
+        print args
+        
+        
 class GetPlayers(Resource):
     def get(self):
         cursor.execute("select * from Player")
@@ -28,6 +56,9 @@ class GetPlayers(Resource):
 
 api.add_resource(GetPlayers, '/getplayers')
 api.add_resource(HelloWorld, '/')
+api.add_resource(Player, '/getplayer')
+
+
 
 
 
