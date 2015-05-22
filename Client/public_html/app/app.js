67dfa40c3e4199d73898de74cd2532653d4bd797
ikhagen <idakhagen@gmail.com>
2015-05-22 16:34:08 +0200

    renamed views

--- Client/public_html/app/app.js
+++ Client/public_html/app/app.js
@@ -30,86 +30,86 @@ app.config(function ($routeProvider) {
     $routeProvider
             .when('/players', {
                 controller: 'PlayersController',
-                templateUrl: 'app/partials/players.html'
+                templateUrl: 'app/partials/playersView.html'
             })
             .when('/editPlayers', {
                 controller: 'PlayersController',
-                templateUrl: 'app/partials/editPlayers.html'
+                templateUrl: 'app/partials/editPlayersView.html'
             })
             .when('/player/:playerId', {
                 controller: 'PlayerController',
-                templateUrl: 'app/partials/player.html'
+                templateUrl: 'app/partials/playerView.html'
             })
             .when('/editPlayer/:playerId', {
                 controller: 'PlayerController',
-                templateUrl: 'app/partials/editPlayer.html'
+                templateUrl: 'app/partials/editPlayerView.html'
             })
             .when('/tournaments', {
                 controller: 'TournamentsController',
-                templateUrl: 'app/partials/tournaments.html'
+                templateUrl: 'app/partials/tournamentsView.html'
             })
             .when('/editTournaments', {
                 controller: 'TournamentsController',
-                templateUrl: 'app/partials/editTournaments.html'
+                templateUrl: 'app/partials/editTournamentsView.html'
             })
             .when('/tournament/:tournamentId', {
                 controller: 'TournamentController',
-                templateUrl: 'app/partials/tournament.html'
+                templateUrl: 'app/partials/tournamentView.html'
             })
             .when('/editTournament/:tournamentId', {
                 controller: 'TournamentController',
-                templateUrl: 'app/partials/editTournament.html'
+                templateUrl: 'app/partials/editTournamentView.html'
             })
             .when('/addTournament', {
                 controller: 'AddTournamentController',
-                templateUrl: 'app/partials/addTournament.html'
+                templateUrl: 'app/partials/addTournamentView.html'
             })
             .when('/matches', {
                 controller: 'MatchesController',
-                templateUrl: 'app/partials/matches.html'
+                templateUrl: 'app/partials/matchesView.html'
             })
             .when('/editMatches', {
                 controller: 'MatchesController',
-                templateUrl: 'app/partials/editMatches.html'
+                templateUrl: 'app/partials/editMatchesView.html'
             })
             .when('/match/:matchId', {
                 controller: 'MatchController',
-                templateUrl: 'app/partials/match.html'
+                templateUrl: 'app/partials/matchView.html'
             })
             .when('/editMatch/:matchId', {
                 controller: 'MatchController',
-                templateUrl: 'app/partials/editMatch.html'
+                templateUrl: 'app/partials/editMatchView.html'
             })
             .when('/addMatch', {
                 controller: 'AddMatchController',
-                templateUrl: 'app/partials/addMatch.html'
+                templateUrl: 'app/partials/addMatchView.html'
             })
             .when('/login', {
-                templateUrl: 'app/partials/login.html'
+                templateUrl: 'app/partials/loginView.html'
             })
             .when('/teams', {
                 controller: 'TeamsController',
-                templateUrl: 'app/partials/teams.html'
+                templateUrl: 'app/partials/teamsView.html'
             })
             .when('/editTeams', {
                 controller: 'TeamsController',
-                templateUrl: 'app/partials/editTeams.html'
+                templateUrl: 'app/partials/editTeamsView.html'
             })
             .when('/team/:teamId', {
                 controller: 'TeamController',
-                templateUrl: 'app/partials/team.html'
+                templateUrl: 'app/partials/teamView.html'
             })
             .when('/editTeam/:teamId', {
                 controller: 'TeamController',
-                templateUrl: 'app/partials/editTeam.html'
+                templateUrl: 'app/partials/editTeamView.html'
             })
             .when('/addTeam', {
                 controller: 'AddTeamController',
-                templateUrl: 'app/partials/addTeam.html'
+                templateUrl: 'app/partials/addTeamView.html'
             })
             .when('/addPlayer', {
                 controller: 'AddPlayerController',
-                templateUrl: 'app/partials/addPlayer.html'
+                templateUrl: 'app/partials/addPlayerView.html'
             })
             .otherwise({redirectTo: '/players'});
 });
